ffff0a95cd948951065289be7866935224c5ffd3
gt.park <smileboy0014@gmail.com>
2023-09-09 15:23:12 +0900

    [Session2. JPA 시작하기 : HelloJpa - 애플리케이션 개발] - 엔터티 매니저 팩토리는 하나만 생성되서 에플리케이션 전체에서 공유(DB 하나당 하나라고 생각하면 편함) - 엔터티 매니저는 요청이 올떄마다 생성되고 버려줘야 한다.(쓰레드간 공유 X) - JPA의 모든 데이터 변경은 트랜잭션 안에서 실행되어야 함 - JPQL이란 엔터티 객체를 대상으로 쿼리

---
+++ ex1_hello_jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,66 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        //트랜잭션 시작
+        tx.begin();
+
+        try {
+            /**
+             * 멤버 저장
+             */
+//            Member member = new Member();
+//
+//            member.setId(1L);
+//            member.setName("helloA");
+//
+//            em.persist(member);
+//            tx.commit();
+            /**
+             * 멤버 조회
+             */
+//            Member findMember = em.find(Member.class, 1L);
+
+            /**
+             * 멤버 수정
+             */
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+//            tx.commit();
+            /**
+             * 멤버 삭제
+             */
+//            Member findMember = em.find(Member.class, 1L);
+//            em.remove(findMember);
+//            tx.commit();
+            /**
+             * JPQL을 이용한 조회
+             */
+//            List<Member> members = em.createQuery("select m from Member m", Member.class)
+//                    .setFirstResult(3)
+//                    .setMaxResults(5)
+//                    .getResultList();
+//            for (Member member : members) {
+//                System.out.println("member = " + member.getName());
+//            }
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
